0fd6923bce0c486049141c25428428b18640a634
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2022-09-15 17:34:54 +0100

    lint errors

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 import { useSelector, useDispatch } from "react-redux";
